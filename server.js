f6670a21e53afc0ce8acdf0333b4c5ef381d163a
dylanR96 <dylan.ravenson@gmail.com>
2024-05-13 16:48:10 +0200

    Added server.js, installed nodemon and created env

---
+++ server.js
@@ -0,0 +1,13 @@
+import http from "http";
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello world!</h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
